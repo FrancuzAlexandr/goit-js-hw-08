5f3897187684f6b361805119e97ecccd3a83e991
FrancuzAlexandr <francuz_s@ukr.net>
2022-11-01 16:22:31 +0200

    2 task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,21 @@
+import Player from '@vimeo/player';
+import throttle from "lodash.throttle";
+
+   
+const iframe = document.querySelector('iframe');
+    const player = new Player(iframe);
+
+    player.on('timeupdate', throttle(function({seconds}) {
+        localStorage.setItem("videoplayer-current-time", seconds)
+        console.log('videoplayer-current-time', seconds);
+ 
+    }),1000);
+
+    const onPlay = Number(localStorage.getItem("videoplayer-current-time"))
+    if (onPlay) {
+        player.setCurrentTime(onPlay)
+    }
+
+
+
 
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,30 @@
+import throttle from "lodash.throttle";
+
+const STORAGE_KEY = "feedback-form-state"
+
+const formEl = document.querySelector("feedback-form");
+
+
+formEl.addEventListener("submit", onFormSubmit);
+formEl.addEventListener("input", throttle(onTextInput, 500));
+
+function onFormSubmit(evt) {
+    evt.preventDefault();
+    evt.currentTarrget.reset();
+    localStorage.removeItem(STORAGE_KEY); 
+}
+
+function onTextInput(evt) {
+    const message = evt.currentTarrget.value;
+    console.log(message);
+    localStorage.setItem(STORAGE_KEY, message)
+}
+
+function populateTextarea() {
+    const savedMessage = localStorage.getItem(STORAGE_KEY);
+    if (savedMessage) {
+        console.log(savedMessage);
+        message.textarea.value = savedMessage
+    }
+}
 
